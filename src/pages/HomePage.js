eec65a78c078e6ae7deaa4e84eaf21f46917d6eb
Larysse Gurgel <larysse.gurgel@sga.pucminas.br>
2022-04-09 21:51:40 -0300

    Novos Arquivos
    Novos Arquivos

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Homepage = () => {
+
+  return (
+
+    <View style={{flex:1, alignItems:'center', justifyContent:'center', backgroundColor:'pink'}}> 
+     
+    <Text> Eu te amo, Andre </Text>
+    
+     </View>
+
+  );
+
+}
+
+export default Homepage;
